9b2352a98d1691ca330d7b8e720b34b4eca82383
Aslly Zuñiga <asllyz2610@gmail.com>
2024-11-14 19:55:12 -0500

    Cretae auth guards

--- Frontend/src/app/create-consumo/create-consumo.component.ts
+++ Frontend/src/app/create-consumo/create-consumo.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-create-consumo',
-  templateUrl: './create-consumo.component.html',
-  styleUrls: ['./create-consumo.component.scss']
+  selector: "app-create-consumo",
+  templateUrl: "./create-consumo.component.html",
+  styleUrls: ["./create-consumo.component.css"],
 })
-export class CreateConsumoComponent implements OnInit {
+export class CreateConsumoComponent {
+  consumoForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.consumoForm = new FormGroup({
+      nombre: new FormControl("", Validators.required),
+      marca: new FormControl("", Validators.required),
+      modelo: new FormControl("", Validators.required),
+      potencia_nominal: new FormControl("", [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"), // Asegura que sea solo numérico
+      ]),
+    });
   }
 
+  onSubmit() {
+    if (this.consumoForm.valid) {
+      console.log("Formulario de consumo enviado:", this.consumoForm.value);
+      // Aquí podrías enviar los datos a una API o servicio.
+    } else {
+      console.log("Formulario no válido");
+    }
+  }
 }
